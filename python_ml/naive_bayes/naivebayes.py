a7ddf8e94049ca50c98f23bb73bf167392d60aa0
Singh, Mohitdeep <mohitdeep.singh@intel.com>
2013-09-28 02:34:00 -0700

    refactoring in process

--- python_ml/naive_bayes/naivebayes.py
+++ python_ml/naive_bayes/naivebayes.py
@@ -5,77 +5,95 @@ import os
 import numpy as np
 """
 Needs further refactoring
-v0.2
+v0.3
 09/28/2013
 Author: Mohitdeep Singh
 """
 
-def load_dictionary(filename="data/dict.txt"):
-    f = open(filename,"r")
-    vector_list = sorted([ line.rstrip(os.linesep) for line in f])
-    return np.ones(len(vector_list),float),vector_list
-    #return np.array([1.0/len(vector_list)]*len(vector_list),float),vector_list
+
+class NaiveBayes:
+    def __init__(self,dictionary = "data/dict.txt"):
+        self._vector_list = self.__load_dictionary(dictionary)
+        self.feature_vector = np.ones(len(self._vector_list),float)
 
 
 
-def generate_likelihood_distributions(document,label,feature_vectors, vector_list):
-    feature_vector = feature_vectors[label]
-    freqs = Counter(document.split())
-    for word, frequency in freqs.items():
-        index = vector_list.index(word.lower())
-        feature_vector[index] +=  float(frequency)
+    def __load_dictionary(self, filename):
+        f = open(filename,"r")
+        vector_list = sorted([ line.rstrip(os.linesep) for line in f])
+        #print "Dictionary loaded!"
+        return vector_list
+
+    def train(self, documents,labels):
+        assert len(documents) == len(labels)
+        feature_vector_dict = self.__setup(labels)
+        #print "training!"
+        for document, label in zip(documents,labels):
+            feature_vector = feature_vector_dict[label]
+            freqs = Counter(document.split())
+            for word, frequency in freqs.items():
+                index = self._vector_list.index(word.lower())
+                feature_vector[index] +=  float(frequency)
+        self.feature_vector_dict = feature_vector_dict
+        self.__normalize_likelihood_vector()
+
+    def test(self,test_document):
+        probs = self.priors.copy()
+        for k,v in probs.items():
+            probs[k] = math.log(v)
+        for word in test_document.split():
+            for label in probs.keys():
+                index = self._vector_list.index(word.lower())
+                probs[label] += math.log(self.feature_vector_dict[label][index])
+        return probs
+
+    def naive_bayes(test_document,priors,likelihood_dict,wordlist):
+        probs = priors.copy()
+        for k,v in probs.items():
+            probs[k] = math.log(v)
+        for word in test_document.split():
+            for label in priors.keys():
+                index = wordlist.index(word.lower())
+                probs[label] += math.log(likelihood_dict[label][index])
+        return probs
+
+    def get_class(self,prob_dict):
+        return sorted(prob_dict.iteritems(), key=operator.itemgetter(1), reverse=True)[0]
+
+    def __setup(self, labels):
+        self.__assign_class_priors(labels)
+        unique_labels = list(set(labels))
+        return {label: self.feature_vector.copy() for label in unique_labels}
+
+    def __assign_class_priors(self, labels):
+        self.priors =  Counter(labels)
+
+    def __normalize_likelihood_vector(self):
+        for label, feature_vector in self.feature_vector_dict.items():
+            self.feature_vector_dict[label] = feature_vector/len(feature_vector)
+
+def main():
+
+    documents = ["This is awesome",
+                 "This is so cool email",
+                 "I won a lottery",
+                 "click to find more information",
+                 "lottery is send email",
+                 "win lottery here",
+                 "click on the link"
+    ]
+    labels = ["ham","ham","ham","ham","spam","spam","spam"]
 
 
-def normalize_likelihood_vector(feature_vector):
-    return feature_vector/len(feature_vector)
 
-def assign_class_priors(labels):
-    return Counter(labels)
+    #----------------------------------------
+    nb = NaiveBayes()
+    nb.train(documents,labels)
+    #print nb.feature_vector_dict
+    test_cases = ["This is amazing","lottery email"]
+    for test_case in test_cases:
+        probs =  nb.test(test_case)
+        print "this document is ", nb.get_class(probs)
 
-def naive_bayes(document,priors,likelihood_dict,wordlist):
-    probs = priors.copy()
-    for k,v in probs.items():
-        probs[k] = math.log(v)
-    for word in document.split():
-        for label in priors.keys():
-            index = wordlist.index(word.lower())
-            probs[label] += math.log(likelihood_dict[label][index])
-    return probs
-
-def get_class(prob_dict):
-    return sorted(prob_dict.iteritems(), key=operator.itemgetter(1), reverse=True)[0]
-
-
-
-"""
-Main code starts from here!
-"""
-
-documents = ["This is awesome",
-             "This is so cool email",
-             "I won a lottery",
-             "click to find more information",
-             "lottery is send email",
-             "win lottery here",
-             "click on the link"
-]
-labels = ["ham","ham","ham","ham","spam","spam","spam"]
-
-
-feature_vector, vector_list = load_dictionary()
-unique_labels = list(set(labels))
-feature_vector_dict = {label: feature_vector.copy() for label in unique_labels}
-
-for label,document in zip(labels,documents):
-    generate_likelihood_distributions(document,label,feature_vector_dict,vector_list)
-
-for label, feature_vector in feature_vector_dict.items():
-    feature_vector_dict[label] = normalize_likelihood_vector(feature_vector)
-
-priors = assign_class_priors(labels)
-
-test_string = "amazing information"
-posteriors =  naive_bayes(test_string,priors,feature_vector_dict,vector_list)
-print "prediction for ", test_string
-print posteriors
-print get_class(posteriors)
+if __name__ == "__main__":
+    main()
